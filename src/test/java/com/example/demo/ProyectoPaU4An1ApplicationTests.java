1d2f7ce25cce5f6c57cda703969609a5e1a03905
anthony <amnaranjos@uce.edu.ec>
2023-02-28 19:45:41 -0500

    Taller 36: Proyecto Web con Spring Boot

---
+++ src/test/java/com/example/demo/ProyectoPaU4An1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPaU4An1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
